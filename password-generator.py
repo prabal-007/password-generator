44d521246b582234fa9a3ea963bf1fb6568a4315
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-06-30 12:10:13 +0530

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -2,14 +2,11 @@ import random
 
 CAP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 low = CAP.lower()
-num = "0123456789"
-symboles = "!@#$%^&*?/><."
+num = str(range(0, 10))
+symbole = "!@#$%^&*?/."
 
-raw = CAP + low + num + symboles
-
+raw = CAP + low + num + symbole
 len = int(input("Enter the length of the password : "))
 
-# password = ''.join(random.choice(raw) for i in range(len))
 password = ''.join(random.sample(raw, len))
-
 print(f"Your password is {password} ")
