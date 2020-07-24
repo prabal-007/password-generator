3ab6240de226bc39ab63807d6ebea878e9b35a59
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-07-25 00:11:26 +0530

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -2,12 +2,6 @@ from tkinter import *
 import random
 import tkinter.messagebox as tmsg
 
-root = Tk()
-
-root.title('Password Generator')
-root.geometry('400x400')
-root.configure(background='orange')
-
 def generate():
     CAP = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     low = CAP.lower()
@@ -55,15 +49,18 @@ def generate():
 def save():
     orgn = orgVar.get()
     strength = staVar.get()
-    if strength == 'weak':
-        value = tmsg.askyesno('weak pass','Your password strength is weak!\nAre sure to set this password?')
-        if value == True:
+    if orgn == '':
+        tmsg.showerror('org field','Enter name of organisation to save pass.')
+    else:
+        if strength == 'weak':
+            value = tmsg.askyesno('weak pass','Your password strength is weak!\nAre sure to set this password?')
+            if value == True:
+                with open('pass-gen.txt','a') as f:
+                    f.write(f'{orgn} - {passvar.get()}\n')
+        else:
             with open('pass-gen.txt','a') as f:
                 f.write(f'{orgn} - {passvar.get()}\n')
-    else:
-        with open('pass-gen.txt','a') as f:
-            f.write(f'{orgn} - {passvar.get()}\n')
-    svaepass.config(state=DISABLED)
+        svaepass.config(state=DISABLED)
 def About():
     root3=Tk()
     root3.geometry('400x400')
@@ -87,7 +84,7 @@ def hel():
     cont.pack(pady='20')
     root2.mainloop()
 def exi():
-    if svaepass(state=DISABLED):
+    if svaepass(state=ACTIVE):
         val1 = tmsg.askyesnocancel('Error','You have not saved your password,\ndo you want to save it?')
         if val1 == 'Yes':
             save()
@@ -98,39 +95,46 @@ def exi():
         if value == True:
             exit()
         else: pass
-Label(root,text='Pass Generator',font='arial 20 bold',padx=5).pack(pady=20)
-frm1 = Frame(root,bg='orange')
-Label(frm1,text='Org - ',font='arial 10 bold', bg='orange').pack(side=LEFT, padx=15)
-orgVar = StringVar()
-Entry(frm1,textvariable=orgVar, font='arial 10', relief=SUNKEN).pack(side=RIGHT)
-frm1.pack()
-frm2 = Frame(root,bg='orange')
-paslen = Label(frm2,text='Pass Length - ', font='arial 10 bold', bg='orange').pack(side=LEFT,padx=10,pady=10)
-lenVer = StringVar()
-Entry(frm2,textvariable=lenVer,font='arial 10', relief=SUNKEN, width='8').pack(side=LEFT,padx=5)
-frm2.pack()
-Label(root,text='*pass length must be in range 4-22.', fg='red', bg='orange').pack()
-Button(root,text='Generate',font='arial 10 bold',pady='2', relief=RAISED,activebackground='gold', command=generate).pack(pady=5)
 
-passvar = StringVar()
-Entry(root,textvariable=passvar, font='arial 15', relief=SUNKEN,width='25', state=DISABLED).pack(pady=10)
-frm3= Frame(root,bg='orange')
-Label(frm3,text='Strength : ', font='helvetica 10',bg='orange').pack(side=LEFT)
-staVar = StringVar()
-strengthlist = ['weak','good','strong','very strong']
-for i in range (len(strengthlist)):
-    radio =Radiobutton(frm3,text=strengthlist[i], variable=staVar,state=DISABLED, value=strengthlist[i],padx='5',bg='orange').pack(side=LEFT)
-frm3.pack()
-svaepass = Button(root,text='Save', font='aral 10 bold', relief=RAISED, activebackground='gold', command=save, state=DISABLED)
-svaepass.pack(pady=5)
-endfrm = Frame(root,bg='gold')
-Label(endfrm,text="By Starkk's", bg='gold').pack(side=RIGHT)
-Label(endfrm,text='©by sta.PG.0.3').pack(side=LEFT)
-endfrm.pack(side=BOTTOM, fill=X)
-frm4 = Frame(root,bg='orange')
-Button(frm4,text='About us',bg='gray', command=About).pack(side=LEFT,padx=5,pady=2)
-Button(frm4,text='Exit',bg='gray', command=exi).pack(side=LEFT,padx=5,pady=2)
-Button(frm4,text='Help',bg='gray', command=hel).pack(side=LEFT,padx=5,pady=2)
+if __name__ == "__main__":    
+    root = Tk()
+    root.title('Password Generator')
+    root.geometry('400x400')
+    root.configure(background='orange')
 
-frm4.pack(side=BOTTOM,fill=X,padx=120)
-root.mainloop()
+    Label(root,text='Pass Generator',font='arial 20 bold',padx=5).pack(pady=20)
+    frm1 = Frame(root,bg='orange')
+    Label(frm1,text='Org - ',font='arial 10 bold', bg='orange').pack(side=LEFT, padx=15)
+    orgVar = StringVar()
+    Entry(frm1,textvariable=orgVar, font='arial 10', relief=SUNKEN).pack(side=RIGHT)
+    frm1.pack()
+    frm2 = Frame(root,bg='orange')
+    paslen = Label(frm2,text='Pass Length - ', font='arial 10 bold', bg='orange').pack(side=LEFT,padx=10,pady=10)
+    lenVer = StringVar()
+    Entry(frm2,textvariable=lenVer,font='arial 10', relief=SUNKEN, width='8').pack(side=LEFT,padx=5)
+    frm2.pack()
+    Label(root,text='*pass length must be in range 4-22.', fg='red', bg='orange').pack()
+    Button(root,text='Generate',font='arial 10 bold',pady='2', relief=RAISED,activebackground='gold', command=generate).pack(pady=5)
+
+    passvar = StringVar()
+    Entry(root,textvariable=passvar, font='arial 15', relief=SUNKEN,width='25', state=DISABLED).pack(pady=10)
+    frm3= Frame(root,bg='orange')
+    Label(frm3,text='Strength : ', font='helvetica 10',bg='orange').pack(side=LEFT)
+    staVar = StringVar()
+    strengthlist = ['weak','good','strong','very strong']
+    for i in range (len(strengthlist)):
+        radio =Radiobutton(frm3,text=strengthlist[i], variable=staVar,state=DISABLED, value=strengthlist[i],padx='5',bg='orange').pack(side=LEFT)
+    frm3.pack()
+    svaepass = Button(root,text='Save', font='aral 10 bold', relief=RAISED, activebackground='gold', command=save, state=DISABLED)
+    svaepass.pack(pady=5)
+    endfrm = Frame(root,bg='gold')
+    Label(endfrm,text="By Starkk's", bg='gold').pack(side=RIGHT)
+    Label(endfrm,text='©by sta.PG.0.3').pack(side=LEFT)
+    endfrm.pack(side=BOTTOM, fill=X)
+    frm4 = Frame(root,bg='orange')
+    Button(frm4,text='About us',bg='gray', command=About).pack(side=LEFT,padx=5,pady=2)
+    Button(frm4,text='Exit',bg='gray', command=exi).pack(side=LEFT,padx=5,pady=2)
+    Button(frm4,text='Help',bg='gray', command=hel).pack(side=LEFT,padx=5,pady=2)
+    frm4.pack(side=BOTTOM,fill=X,padx=120)
+
+    root.mainloop()
