c65d6346bc987b21f55bfae12e0ddbb6a1982b67
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-07-24 22:08:37 +0530

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,16 +1,3 @@
-# # import random
-
-# CAP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-# low = CAP.lower()
-# num = str(range(0, 10))
-# symbole = "!@#$%^&*?/."
-
-# raw = CAP + low + num + symbole
-# len = int(input("Enter the length of the password : "))
-
-# password = ''.join(random.sample(raw, len))
-# print(f"Your password is {password} ")
-
 from tkinter import *
 import random
 
@@ -31,39 +18,37 @@ def generate():
     raw3 = low+num
     raw4 = num+symbole
     raw5 = low+symbole
-    len = int(lenVer.get())
-    pass1 = ''.join(random.sample(raw1,len))
-    pass2 = ''.join(random.sample(raw2,len))
-    pass3 = ''.join(random.sample(raw3,len))
-    pass4 = ''.join(random.sample(raw4,len))
-    pass5 = ''.join(random.sample(raw5,len))
-    strength = staVar.get()
-    if strength == 'weak':
-        passvar.set(pass3)
-    elif strength == 'good':
-        passvar.set(random.choice(pass4,pass5))
-    elif strength == 'strong':
-        passvar.set(pass2)
-    else:
-        passvar.set(pass1)
     
+    try:
+        len = int(lenVer.get())
+        pass1 = ''.join(random.sample(raw1,len))
+        pass2 = ''.join(random.sample(raw2,len))
+        pass3 = ''.join(random.sample(raw3,len))
+        pass4 = ''.join(random.sample(raw4,len))
+        pass5 = ''.join(random.sample(raw5,len))
+        strength = staVar.get()
+        if len < 7:
+            staVar.set('weak')
+            print(strength)
+            passvar.set(pass3)
+        elif 6<len<11:
+            staVar.set('good')
+            lis = [pass4,pass5]
+            passvar.set(random.choice(lis))
+        elif 10<len<16:
+            staVar.set('strong')
+            passvar.set(pass2)
+        elif 15<len<23:
+            staVar.set('very strong')
+            passvar.set(pass1)
+        else:
+            passvar.set('Error')
+    except Exception as e:
+        passvar.set('Error')
     # passlist = [pass1,pass2,pass3]
     # password = random.choice(passlist)
     # passvar.set(password)
     # strength(password)
-
-# def strength(password):
-#     etr = Label(root,text='wer', font='arial 10')
-#     etr.pack()
-#     strengthlist = ['Enter Something','weak','good','strong','very strong']
-#     score = 1
-#     if len(password) < 6:
-#         etr.config(text = strengthlist[1])
-#         # etr.update()
-#         return
-#     else:
-#         pass
-
 
 Label(root,text='Pass Generator',font='arial 20 bold',padx=5).pack(pady=20)
 frm1 = Frame(root,bg='orange')
@@ -74,19 +59,19 @@ frm1.pack()
 frm2 = Frame(root,bg='orange')
 paslen = Label(frm2,text='Pass Length - ', font='arial 10 bold', bg='orange').pack(side=LEFT,padx=10,pady=10)
 lenVer = StringVar()
-Entry(frm2,textvariable=lenVer,font='arial 10', relief=SUNKEN, width='12').pack(side=RIGHT,padx=10)
+Entry(frm2,textvariable=lenVer,font='arial 10', relief=SUNKEN, width='8').pack(side=LEFT,padx=5)
 frm2.pack()
+Label(root,text='*pass length must be in range 4-23.', fg='red', bg='orange').pack()
 Button(root,text='Generate',font='arial 10 bold',pady='2', relief=RAISED,activebackground='gold', command=generate).pack(pady=5)
+
+passvar = StringVar()
+Entry(root,textvariable=passvar, font='arial 15', relief=SUNKEN, state=DISABLED).pack(pady=10)
 frm3= Frame(root,bg='orange')
 Label(frm3,text='Strength : ', font='helvetica 10',bg='orange').pack(side=LEFT)
 staVar = StringVar()
-staVar.set('weak')
+# staVar.set('weak')
 strengthlist = ['weak','good','strong','very strong']
 for i in range (len(strengthlist)):
-    radio =Radiobutton(frm3,text=strengthlist[i], variable=staVar, value=strengthlist[i],padx='5',bg='orange').pack(side=LEFT)
+    radio =Radiobutton(frm3,text=strengthlist[i], variable=staVar,state=DISABLED, value=strengthlist[i],padx='5',bg='orange').pack(side=LEFT)
 frm3.pack()
-passvar = StringVar()
-Entry(root,textvariable=passvar, font='arial 15', relief=SUNKEN).pack(pady=10)
-
-
 root.mainloop()
